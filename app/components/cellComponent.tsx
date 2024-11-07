ee9e444a6a60f4e3f116ff786994911c1dd80dd9
Abram-Emad <ArtistArtistAbram2015@outlook.com>
2024-11-07 22:21:20 +0200

    Uploding-React.js-Tic-Tac-Toe-Application

---
+++ app/components/cellComponent.tsx
@@ -0,0 +1,50 @@
+import React, { Dispatch, SetStateAction } from "react";
+
+type CellProps = {
+  id: number;
+  go: string;
+  setGo: Dispatch<SetStateAction<string>>;
+  cells: string[];
+  setCells: Dispatch<SetStateAction<string[]>>;
+  cell: string;
+  winningMessage: string;
+};
+
+function CellComponent({
+  id,
+  go,
+  setGo,
+  cells,
+  setCells,
+  cell,
+  winningMessage,
+}: CellProps) {
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (winningMessage) return;
+
+    const notTaken = !cells[id];
+    if (notTaken) {
+      if (go === "circle") {
+        handleCellChange("circle");
+        setGo("cross");
+      } else if (go === "cross") {
+        handleCellChange("cross");
+        setGo("circle");
+      }
+    }
+  };
+
+  const handleCellChange = (cellToChange: string) => {
+    const copyCells = [...cells];
+    copyCells[id] = cellToChange;
+    setCells(copyCells);
+  };
+
+  return (
+    <div className='square' onClick={handleClick}>
+      <div className={cell}>{cell ? (cell === "circle" ? "O" : "X") : ""}</div>
+    </div>
+  );
+}
+
+export default CellComponent;
